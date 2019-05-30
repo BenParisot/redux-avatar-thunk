424f36b36694185d59762c15468de43fa4003dc9
Ben Parisot <me@benparisot.com>
2019-05-30 14:35:19 -0700

    write router and switch

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import Home from './Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import CharacterDetail from '../components/characters/CharacterDetail';
 
 export default function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/characters/:id" component={CharacterDetail} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  ); 
 }
---
+++ src/components/characters/CharacterDetail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function CharacterDetail({ character }) {
+  return (
+    <section>
+      <h2>{character.name}</h2>
+      <img src={character.photoUrl} />
+    </section>
+  );
+}
+CharacterDetail.propTypes = {
+  character: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    photoUrl: PropTypes.string.isRequired,
+    _id: PropTypes.string.isRequired
+  })
+};
+
+export default CharacterDetail;
